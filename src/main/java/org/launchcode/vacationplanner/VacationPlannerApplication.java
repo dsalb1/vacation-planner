376593f1f5fbd55ff358619e808149260af0514e
Dan Salberg <daniel.j.salberg@gmail.com>
2017-07-01 13:20:03 -0500

    Initial commit.

---
+++ src/main/java/org/launchcode/vacationplanner/VacationPlannerApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.vacationplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPlannerApplication.class, args);
+	}
+}
