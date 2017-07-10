f75a16fdcab2113ebc3b063781db91f1201d0ecd
Dan Salberg <daniel.j.salberg@gmail.com>
2017-07-09 21:55:03 -0500

    Created user class and SignUpController. Built template form for user signup.

--- src/main/java/org/launchcode/vacationplanner/Controllers/SignUpController.java
+++ src/main/java/org/launchcode/vacationplanner/Controllers/SignUpController.java
@@ -1,7 +1,24 @@
 package org.launchcode.vacationplanner.Controllers;
 
+import org.launchcode.vacationplanner.Models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Dan on 7/5/2017.
  */
+
+@Controller
+@RequestMapping(value="vacation/user")
 public class SignUpController {
+
+
+    @RequestMapping(value="signup")
+    public String userSignUp(Model model) {
+        model.addAttribute("title", "User Sign Up");
+        model.addAttribute("user", new User());
+
+        return "user/signup";
+    }
 }
